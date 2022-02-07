90a4be29ed61d3e8a5b1392c015b5054f0672444
Andrew Dodson <andrew@5app.com>
2022-02-07 13:02:41 +0000

    feat(init): initial semantic-release-config

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 /**
- * Entry point to initiate the project
+ * Export Default Semantic Release shareable config
  */
+module.exports = {
+	branches: ['main', 'master'],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		'@semantic-release/npm',
+		'@semantic-release/git',
+		'@semantic-release/github',
+	],
+};
